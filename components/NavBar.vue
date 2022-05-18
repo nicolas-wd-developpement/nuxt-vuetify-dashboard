c23c3813c63e54526f20610d18fa288518088ec4
nicolasblond@web-designers.fr <nicolasblond@web-designers.fr>
2022-05-18 06:52:29 +0200

    Add dashBoard page

---
+++ components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+<!-- Nav Bar -->
+      <v-app-bar app color="primary" dark>
+        <v-toolbar-title> Vuetify toolbar title</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn text rounded to="/">Home</v-btn>
+         <v-btn text rounded to="/dashBoard">Dash Board</v-btn>
+        <v-btn text rounded to="/login">Login</v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+     name: 'NavBar',
+     
+    setup () {
+        
+
+        return {}
+    },
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -5,14 +5,33 @@
     <v-content >
     <v-card black>
         <v-card-title  width="1900" class="mx-auto mt-5 white">
-            <h1 class="display-1"> Home Page</h1>
+            <h1 class="display-1"> Login Page</h1>
         </v-card-title>
     </v-card>
       <!-- lOGIN modal-->
       <v-card width="400" class="mx-auto mt-5 black">
         <v-card-title>
-                <h2 class="display-2"> WELCOME</h2>
+                <h2 class="display-2"> Login</h2>
         </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field label="UserName" prepend-icon="mdi-account-circle"></v-text-field>
+            <v-text-field 
+              :type= "showPassword ? 'text' : 'password'"
+              label="password"
+              prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye' :'mdi-eye-off'"
+              @click:append="showPassword = ! showPassword">
+            </v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+
+          <v-btn color="success"> Register </v-btn>
+                <v-spacer>   </v-spacer>
+          <v-btn color="info"> Login </v-btn>
+        </v-card-actions>
       </v-card>
     </v-content>
     <FooterComponent></FooterComponent>
